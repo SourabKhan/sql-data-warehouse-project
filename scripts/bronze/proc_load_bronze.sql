8db40e55d07600fbaa8df60a922925fbe74ed1c1
Sourab Khan <108218692+SourabKhan@users.noreply.github.com>
2026-01-22 12:48:35 +0530

    Refine comments in proc_load_bronze.sql
    Updated comments for clarity and formatting.

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -8,12 +8,12 @@ It performs the following actions:
 - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
 
 Parameters:
-None. This stored procedure does not accept any parameters or return any values.
+This stored procedure does not accept any parameters or return any values.
 
 Usage Example:
 EXEC bronze.load_bronze;
-
 */
+
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
